46af5ce565afab0bf6884b3ec4db1c5607e2d7c7
Jason Chaney <jason@creativeacceleration.com>
2011-09-12 11:41:58 -0700

    Form is now coded but not tested.  Next is to finish pre-population button for testing, then fix issues then move to data handeling.  Localization for layout can begin once art is supplied.
    Signed-off-by: Jason Chaney <jason@creativeacceleration.com>

--- js/script.js
+++ js/script.js
@@ -14,11 +14,11 @@ window.log = function() {
 
 var formats = {
 	English : {
-		format: "$#,###",
+		format: "#,###.00",
 		locale: "us"
 	},
 	German : {
-		format: "&euro;#,###",
+		format: "#,###.00",
 		locale: "de"
 	}
 };
@@ -45,6 +45,11 @@ function validateData(vo) {
 function formatAmericanNumberToLanguage(sourceAmericanNumber, language) {
 	var isoNum = $.parseNumber(String(sourceAmericanNumber), formats.English);
 	return $.formatNumber(isoNum, formats[language]);
+}
+
+function convertNumberFormat(sourceNumber, sourceLanguage, destinationLanguage) {
+	var isoNum = $.parseNumber(String(sourceNumber), formats[sourceLanguage]);
+	return $.formatNumber(isoNum, formats[destinationLanguage]);
 }
 
 
@@ -110,6 +115,9 @@ function getHeightFromPercent(percent) {
 }
 
 function remove() {
+
+	//Remove visualization
+
 	$('#blueContent, #orangeContent, #d_roomsBase, #e_packageBase, #f_opaque').stop().animate({
 		bottom:'-300px'
 	}, 1500);
@@ -117,13 +125,113 @@ function remove() {
 	$('#roomsNeededCont').stop().animate({
 		opacity:0
 	}, 1500);
+
+
+	//Add code to clear fields
+
 }
 
+function calculate(){
 
-//------------------------------------------------END OTA Visualization COde
+	//Note var namnes match /comps/formulas.png
+
+	//First, convert everythign to American for math operations
+	var A = convertNumberFormat($('#A').val(), $("#language").val(), "English");
+	var B = convertNumberFormat($('#B').val(), $("#language").val(), "English");
+	var C = convertNumberFormat($('#C').val(), $("#language").val(), "English");
+	var D = convertNumberFormat($('#D').val(), $("#language").val(), "English");
+	var E = convertNumberFormat($('#E').val(), $("#language").val(), "English");
+	var F = convertNumberFormat($('#F').val(), $("#language").val(), "English");
+	var G = convertNumberFormat($('#G').val(), $("#language").val(), "English");
+	var H = convertNumberFormat($('#H').val(), $("#language").val(), "English");
+	var J = convertNumberFormat($('#J').val(), $("#language").val(), "English");
+	var K = convertNumberFormat($('#K').val(), $("#language").val(), "English");
+	var L = convertNumberFormat($('#L').val(), $("#language").val(), "English");
+	var M = convertNumberFormat($('#M').val(), $("#language").val(), "English");
+	var N = convertNumberFormat($('#N').val(), $("#language").val(), "English");
+	var P = convertNumberFormat($('#P').val(), $("#language").val(), "English");
+	var Q = convertNumberFormat($('#Q').val(), $("#language").val(), "English");
+	var R = convertNumberFormat($('#R').val(), $("#language").val(), "English");
+
+	//Next, generate the calculated values
+
+	var CE = C/E;
+	var HG = H/G;
+	var U = (G/S)*J;
+	var LK = L/K;
+	var V = (K/S)*M;
+	var PN = P/N;
+	var W = (N/S)*Q;
+	var S = Number(G)+Number(K)+Number(N);
+	var T = Number(H)+Number(L)+Number(P);
+	var TS = T/S;
+	var Z = Number(U)+Number(V)+Number(W);
+	var b_ = (Number(G)+Number(K)+Number(N))*R;
+	var X = R*T;
+	var Y = F*S;
+
+	//Calculate the value object the visualization needs here. Copy still needs to be pulled from global JSON (which is created from the Excel language matrix)
+
+	var vo = {
+		language: $("#language").val(),
+		copy: {
+			V_L_ota_profit		: "OTA PROFIT",
+			V_T_concerns		: "CONCERNS",
+			V_T_p1a				: "Do these numbers look good to you?",
+			V_T_p1b				: "Are you willing to live with this flow through rate?",
+			V_T_p2a				: "What can you do differently?",
+			V_T_p2b				: "Will you hit your targets given these profit numbers?",
+			T_ota_total_cont	: "OTA TOTAL ROOM CONTRIBUTION",
+			T_ota_inc_rm_cont	: "OTA INCREMENTAL ROOM CONTRIBUTION",
+			L_your_profit		: "YOUR PROFIT",
+			T_rms_needed		: "ROOMS NEEDED ON DIRECT CHANNELS TO REPLACE PROFITS",
+			T_rooms_only		: "Rooms Only",
+			T_package			: "Package",
+			T_opaque			: "Opaque"
+		},
+		OTAProfitAmount: Z*T,
+		percentPackage: (M*L)/(Z*T),
+		percentRooms: (H*J)/(Z*T),
+		percentOpaque: (Q*P)/(Z*T),
+		yourProfit: X-Y,
+		rightBldgPercent: 50, //Figure this out
+		roomsNeeded: A/(((H*J)/(Z*T))-((Q*P)/(Z*T))),
+		otaRoomContPercentage: S/E,
+		otaIncreRoomContPercent: b_/E
+	}
+
+	//Finally, populate the fields with the correct formatting and launch visualization
+
+	$('#CE').html(  convertNumberFormat(CE, "English", $("#language").val()));
+	$('#HG').html(  convertNumberFormat(HG, "English", $("#language").val()));
+	$('#U').html(  convertNumberFormat(U, "English", $("#language").val()));
+	$('#LK').html(  convertNumberFormat(LK, "English", $("#language").val()));
+	$('#V').html(  convertNumberFormat(V, "English", $("#language").val()));
+	$('#PN').html(  convertNumberFormat(PN, "English", $("#language").val()));
+	$('#W').html(  convertNumberFormat(W, "English", $("#language").val()));
+	$('#S').html(  convertNumberFormat(S, "English", $("#language").val()));
+	$('#T').html(  convertNumberFormat(T, "English", $("#language").val()));
+	$('#TS').html(  convertNumberFormat(TS, "English", $("#language").val()));
+	$('#Z').html(  convertNumberFormat(Z, "English", $("#language").val()));
+	$('#b_').html(  convertNumberFormat(b_, "English", $("#language").val()));
+	$('#X').html(  convertNumberFormat(X, "English", $("#language").val()));
+	$('#Y').html(  convertNumberFormat(Y, "English", $("#language").val()));
+
+	//Send the visualization object to init
+
+	init(vo);
+
+}
+
+//This is used for development to prepopulate the form with English FPO data
+function populateFPO(){
+	$('#A').val(123);
+
+}
+
 //Sample object
-var sample = {
-	language: "German",
+/*var sample = {
+	language: "English",
 	copy: {
 		V_L_ota_profit		: "OTA PROFIT",
 		V_T_concerns		: "CONCERNS",
@@ -148,13 +256,25 @@ var sample = {
 	roomsNeeded: 388, //Included to keep all calculations in one place
 	otaRoomContPercentage: 6.8,
 	otaIncreRoomContPercent: 2.3
-};
+};*/
 
 $(document).ready(function() {
-	$('#animateIn').bind('click', function() {
-		init(sample);
+
+
+	$('#calculateBTN').bind('click', function(e){
+		calculate();
 	});
-	$('#animateOut').bind('click', function() {
+
+	$('#symbol').bind('change', function(e){
+		$(".currency").html(
+				$(e.currentTarget).val()
+				);
+	})
+
+	$('#animateIn').bind('click', function() {
+		populateFPO();
+	});
+	$('#clearBTN').bind('click', function() {
 		remove();
 	});
 });
